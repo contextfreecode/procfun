164e7967148be5e0d5b625fd7ce2e2537a118f55
Tom <tom@contextfree.info>
2021-11-16 06:30:58 -0700

    Start ts; tweak wording

---
+++ guess.ts
@@ -0,0 +1,29 @@
+type int = number;
+
+type Game = {
+  answer: int;
+  guesses: int;
+  high: int;
+};
+
+function askGuess(high: int) {
+  return prompt(`Guess a number between 1 and ${high}:`);
+}
+
+function pickAnswer(high: int) {
+  return crypto.getRandomValues(new Uint32Array(1))[0] % high;
+}
+
+function play(game: Game) {
+  const guess = askGuess(game.high);
+  return guess;
+}
+
+function main() {
+  const high = 100;
+  const answer = pickAnswer(high);
+  const game = {answer, guesses: 0, high};
+  play(game);
+}
+
+main();
