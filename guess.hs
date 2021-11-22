e843634bd01fa4c3a5b24ec6d99106445a6f0076
Tom <tom@contextfree.info>
2021-11-22 07:15:42 -0700

    Finally type the thing

--- guess.hs
+++ guess.hs
@@ -7,21 +7,20 @@ pickAnswer :: Int -> IO Int
 pickAnswer high =
     randomRIO (1, high)
 
--- p2 :: StatefulGen g m => Int g -> m Int
--- p2 :: (Random a, Control.Monad.IO.Class.MonadIO m, Num a) => a -> p -> m a
-p2 high =
+pickAnswer2 :: RandomGen gen => Int -> gen -> (Int, gen)
+pickAnswer2 high =
     uniformR (1, 10 :: Int)
 
 main :: IO ()
 main = do
     answer <- pickAnswer high
-    v2 <- getStdRandom $ p2 10
+    v2 <- getStdRandom $ pickAnswer2 10
     print answer
     print val
     print v2
     print v3
     where
         high = 100
-        rng = mkStdGen 5
-        v3 = p2 10 rng
+        gen = mkStdGen 5
+        v3 = pickAnswer2 10 gen
         val = uniformR (1, 10 :: Int) (mkStdGen 5)
