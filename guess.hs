cfc2cc30625d3a8160c86c5548ddaa4a0c196f76
Tom <tom@contextfree.info>
2021-11-23 07:31:10 -0700

    Make progress

--- guess.hs
+++ guess.hs
@@ -3,6 +3,23 @@ import System.Clock
 import System.Random
 import System.Random.Stateful
 
+data Game = Game {
+    answer :: Int,
+    done :: Bool,
+    guesses :: Int,
+    high :: Int
+}
+
+gameAnswer = answer
+gameHigh = high
+
+gameDefault = Game {
+    answer = undefined,
+    done = False,
+    guesses = 0,
+    high = undefined
+}
+
 pickAnswer :: Int -> IO Int
 pickAnswer high =
     randomRIO (1, high)
@@ -13,6 +30,10 @@ pickAnswer2 high = uniformR (1, high)
 pickAnswer3 :: StatefulGen gen m => Int -> gen -> m Int
 pickAnswer3 high = uniformRM (1, high)
 
+play :: Game -> IO Game
+play game = do
+    return game
+
 timeSeed :: IO Int
 timeSeed = do
     time <- getTime Monotonic
@@ -20,18 +41,22 @@ timeSeed = do
 
 main :: IO ()
 main = do
-    seed <- timeSeed
-    let g3 = mkStdGen seed
-    let (v5, g4) = pickAnswer2 100 g3
     answer <- pickAnswer high
-    v2 <- getStdRandom $ pickAnswer2 100
+    let game = gameDefault { answer = answer, high = high }
+    result <- play game
     print answer
-    print val
-    print v2
-    print v3
-    -- v4 <- pickAnswer3 100 (newIOGenM gen)
-    -- print v4
-    print v5
+    -- seed <- timeSeed
+    -- let g3 = mkStdGen seed
+    -- let (v5, g4) = pickAnswer2 100 g3
+    -- -- See https://hackage.haskell.org/package/random-1.2.1/docs/src/System.Random.html#getStdRandom
+    -- v2 <- getStdRandom $ pickAnswer2 100
+    -- print answer
+    -- print val
+    -- print v2
+    -- print v3
+    -- -- v4 <- pickAnswer3 100 (newIOGenM gen)
+    -- -- print v4
+    -- print v5
     where
         high = 100
         gen = mkStdGen 5
