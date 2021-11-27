c2dc4518467ef2f8054da89bccf204424bc9115d
Tom <tom@contextfree.info>
2021-11-27 07:49:11 -0700

    Start game

--- guess.rs
+++ guess.rs
@@ -1,3 +1,21 @@
+use rand::Rng;
+
+#[derive(Clone, Copy)]
+struct Game {
+    answer: i32,
+    done: bool,
+    guesses: i32,
+    high: i32,
+}
+
+fn play(game: Game) -> Game {
+    game
+}
+
 fn main() {
-    //
+    let high = 100;
+    let mut rng = rand::thread_rng();
+    let answer = rng.gen_range(1..=high);
+    let game = Game { answer, done: false, guesses: 0, high };
+    let game = play(game);
 }
